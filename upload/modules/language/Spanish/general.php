483c38f5460fdeb6c28a1b245900cd9693cc4d5b
BrayaanPS <30274810+BrayaanPS@users.noreply.github.com>
2021-01-01 14:10:55 -0300

    Create general.php

---
+++ upload/modules/language/Spanish/general.php
@@ -0,0 +1,30 @@
+<?php
+/*
+ *	Made by Partydragen
+ *  https://github.com/partydragen/Nameless-Suggestions
+ *  https://partydragen.com/
+ *
+ *  EnglishUK Language for Suggestions module
+ */
+
+$language = array(
+	'suggestions' => 'Sugerencias',
+	'no_suggestions' => 'No se encontraron sugerencias.',
+	'new_suggestion' => 'Nueva sugerencia',
+	'none_statuses_defined' => 'No se ha encontrado ningún estado',
+	'none_categories_defined' => 'No se ha encontrado ninguna categoría',
+	'title' => 'Título',
+	'last_reply' => 'Última respuesta',
+	'stats' => 'Estadísticas',
+	'likes' => 'Me gusta',
+	'dislikes' => 'No me gusta',
+	'categories' => 'Categorías',
+	'category' => 'Categoría',
+	'content' => 'Contenido',
+	'recent_activity' => 'Actividad reciente',
+	'search_keyword' => 'Buscar palabra clave',
+	'sort_by' => 'Ordenar por',
+	'newest' => 'El más reciente',
+	'comments' => 'Comentarios',
+	'new_comment' => 'Nuevo comentario',
+);
